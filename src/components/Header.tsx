306d562cacbaea3b1fe134e80f5ea8c84bf336a7
Joni Rämö <joni.ramo@houston-inc.com>
2021-11-22 19:22:46 +0200

    Style Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import { ReactComponent as CowFace } from '../assets/lehma.svg';
 import { ReactComponent as MilkMateLogo } from '../assets/milkmate_logo.svg';
+import styled from 'styled-components';
 
 type HeaderProps = { username: string | undefined };
 
+const StyledHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0.5em 1em;
+  background: #1d4485;
+`;
+
 const Header = ({ username }: HeaderProps) => {
   return (
-    <header>
-      <CowFace />
-      <MilkMateLogo />
-      <div>{username}</div>
-    </header>
+    <StyledHeader>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+        }}>
+        <CowFace style={{ width: 50, height: 50, fill: 'white' }} />
+        <MilkMateLogo
+          style={{ width: 125, height: 30, fill: 'white', marginLeft: '1em' }}
+        />
+      </div>
+      <div style={{ color: 'white' }}>{username}</div>
+    </StyledHeader>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 
 import App from './views/App';
 import reportWebVitals from './reportWebVitals';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    font-family: sans-serif;
+  }
+`;
 
 // Here I would add any Providers I use in the application.
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
